0d684d684689dc261a87240bb5a97ec90b78e1d9
Chandrahash Kumar <chandrahashkumar256@gmail.com>
2025-04-10 23:18:08 +0530

    try and except

---
+++ 100425/2.py
@@ -0,0 +1,9 @@
+from typing import final
+
+a = input("Enter two number: ").split()
+try:
+    c = int(a[0]) + int(a[1])
+    print(c)
+except ValueError:
+    print("Invalid input")
+print("Thank you for using this program")
