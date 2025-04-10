febed15e8508d595b5848e4a743704b6dd7418a6
Chandrahash Kumar <chandrahashkumar256@gmail.com>
2025-04-10 23:26:18 +0530

    add more code

--- 100425/2.py
+++ 100425/2.py
@@ -1,7 +1,13 @@
 a = input("Enter two number: ").split()
 try:
     c = int(a[0]) + int(a[1])
+    c = int(a[0]) / int(a[1])
+    K = "kali" + int(a[0])
     print(c)
 except ValueError:
-    print("Invalid input")
+    print("ValueError")
+except TypeError:
+    print("TypeError")
+except ZeroDivisionError:
+    print("ZeroDivisionError")
 print("Thank you for using this program")
