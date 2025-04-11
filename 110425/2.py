6f804e3d39175b1ffcb0e2b2df62ddcf721f84d8
Chandrahash Kumar <chandrahashkumar256@gmail.com>
2025-04-11 21:46:28 +0530

    add error handling

--- 110425/2.py
+++ 110425/2.py
@@ -1,9 +1,16 @@
-# How to print, which type of error generate
-try:
-    n = int(input())
-    print(n)
-except:
-    print("Error")
+# # How to print, which type of error generate
+# try:
+#     n = int(input())
+#     print(n)
+# except:
+#     print("Error")
+#
+# finally:
+#     print("OK")
 
-finally:
-    print("OK")
+try:
+   with open('data.csv') as file:
+       read_data = file.read()
+except FileNotFoundError as fnf_error:
+   print(fnf_error)
+   print("Explanation: We cannot load the 'data.csv' file")
