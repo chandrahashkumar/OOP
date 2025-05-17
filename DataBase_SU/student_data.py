22468da9e93cdcfb0806a9901c99a76d5b230f3d
Chandrahash Kumar <chandrahashkumar256@gmail.com>
2025-05-18 00:11:47 +0530

    student_database update

--- DataBase_SU/student_data.py
+++ DataBase_SU/student_data.py
@@ -3,10 +3,11 @@ con =mysql.connector.connect(host='localhost',database='universitydb',user='root
 ors = con.cursor()
 #ors.execute("CREATE TABLE STUDENT_DATA(PRN VARCHAR(12) PRIMARY KEY, NAME VARCHAR (50),SEX VARCHAR(20),BRANCH VARCHAR(100),EMAIL_ID VARCHAR(100) UNIQUE,MOBILE BIGINT,ADMISSION_DATE DATE)")
 # ors.execute("ALTER TABLE STUDENT_DATA RENAME COLUMN MOBILE TO MOBILE_NUMBER ")
-print("START OF PROGRAM TO PRESS ANY KEY")
+print("____WELCOME TO STUDENT DATABASE____")
+print("PRESS ANY KEY TO CONTINUE")
 input()
 while True:
-    print("\n1. Insert new data\n2. Search data\n3. Delete data\n4. Exit")
+    print("\n1. ADD New Student\n2. Display Students List\n3. Delete data\n4. Exit")
     choice = int(input("Enter your choice: "))
     if choice == 1:
         s = int(input("Enter number of students you want to enter: "))
@@ -19,11 +20,11 @@ while True:
             Mobile = int(input("Enter Mobile Number: "))
             Admission_Date = input("Enter Admission Date(YYYY-MM-DD): ")
             ors.execute(f"INSERT INTO STUDENT_DATA(PRN,Name,Sex,Branch,Email_ID,Mobile_Number,Admission_Date) VALUES ('{PRN}','{Name}','{Sex}','{Branch}','{Email}','{Mobile}','{Admission_Date}') ")
-
     if choice == 2:
         ors.execute("SELECT * FROM STUDENT_DATA")
         d = ors.fetchall()
         print(d)
     elif choice == 4:
+        print("Thanks for using our service")
         break
 
