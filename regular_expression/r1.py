1ebc114f82191c87a8cef398290b78baf8f5dcfa
Chandrahash Kumar <chandrahashkumar256@gmail.com>
2025-05-20 23:44:52 +0530

    code update

--- regular_expression/r1.py
+++ regular_expression/r1.py
@@ -1,7 +1,8 @@
 import re
 
 file = open('AI.txt',)
+
 for line in file:
     line = line.rstrip()
-    if re.search('^Artificial',line):
+    if re.search('^Artificial', line):
         print(line)
