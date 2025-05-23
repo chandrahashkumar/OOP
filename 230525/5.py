84225465afe05c588a7e827c9622261632963964
Chandrahash Kumar <chandrahashkumar256@gmail.com>
2025-05-23 17:13:51 +0530

    object delete

--- 230525/5.py
+++ 230525/5.py
@@ -7,6 +7,7 @@ class Animal:
 c = Animal('Cat',1)
 c.age = 2   # modify object properties , Here set Cat age 1 to 2
 print(c.age)
+
 del c.age  # Delete the age property from the c object
 del c # Delete the object
 
