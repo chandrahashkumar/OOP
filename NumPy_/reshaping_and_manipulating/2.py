4b738357411eb650c38257ef58f2d4d704f9168a
Chandrahash Kumar <chandrahashkumar256@gmail.com>
2025-05-25 15:38:05 +0530

    multi D to 1D

---
+++ NumPy_/reshaping_and_manipulating/2.py
@@ -0,0 +1,10 @@
+"""
+.ravel() - view
+.flatten()  - copy
+"""
+import numpy as np
+arr_2d = np.array([[4,6,9],
+                   [7,9,3]])
+
+print(arr_2d.ravel()) # multi D TO 1 D
+print(arr_2d.flatten()) # multi D TO 1 D
