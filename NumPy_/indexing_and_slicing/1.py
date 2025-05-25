f341fdb44c46cd6ad3a4255fb9a0aa5f66a7f30c
Chandrahash Kumar <chandrahashkumar256@gmail.com>
2025-05-25 14:44:36 +0530

    indexing

---
+++ NumPy_/indexing_and_slicing/1.py
@@ -0,0 +1,13 @@
+import numpy as np
+#1D ARRAY
+arr = np.array([10,20,30,4,1,8,9,696,55])
+
+print(arr[0])
+print(arr[-1])
+
+# 2D ARRAY
+arr2d = np.array([[4,6,3],
+                  [7,9,3]])
+
+print(arr2d[1][1]) #9
+print(arr2d[0][2]) #3
