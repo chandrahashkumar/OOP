8092f560be0ca6e0abb1dcaa00f0579adfaf0ed5
Chandrahash Kumar <chandrahashkumar256@gmail.com>
2025-05-30 16:18:00 +0530

    Encapsulation

---
+++ 300525.py
@@ -0,0 +1,20 @@
+# Encapsulation
+class Bank:
+    def __init__(self,Name):
+        self.__Name = Name
+        self.__account_no = 45785
+        self.__customerID = 56
+    def display(self):
+        print(f"Account Number {self.__account_no}")
+    def __change_name(self,Name):
+        self.__Name =Name
+        print(self.__Name)
+
+c = Bank("Kali")
+# print(c.Name)
+print(c._Bank__customerID)
+c._Bank__customerID = 78
+print(c._Bank__customerID)
+c.display()
+c._Bank__change_name("KAL")
+print(c._Bank__Name)
