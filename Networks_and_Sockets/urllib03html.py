9a2352f2f9ee01a4e882af410bf4269a8e50114a
Chandrahash Kumar <chandrahashkumar256@gmail.com>
2025-06-03 13:26:07 +0530

    open html file

---
+++ Networks_and_Sockets/urllib03html.py
@@ -0,0 +1,17 @@
+import urllib.request, urllib.parse, urllib.error
+
+fhand = urllib.request.urlopen('http://www.dr-chuck.com/page1.htm')
+for line in fhand:
+    print(line.decode().strip())
+
+
+# Output
+'''
+<h1>The First Page</h1>
+<p>
+If you like, you can switch to the
+<a href="http://www.dr-chuck.com/page2.htm">
+Second Page</a>.
+</p>
+
+'''
